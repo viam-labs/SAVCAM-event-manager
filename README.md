6727585dcc1fae19cbdeddef9d3365419f6f9866
Matt Vella <mcvella@gmail.com>
2023-12-08 12:14:11 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# SAVCAM-event-manager
+# event-manager modular service
+
+*event-manager* is a Viam modular service that provides generic capabilities
+
+The model this module makes available is viam-labs:savcam:event-manager
+
+## Prerequisites
+
+Add instructions here for any prerequisites.
+
+``` bash
+```
+
+## API
+
+The event-manager resource implements the [rdk generic API](https://github.com/rdk/generic-api).
+
+Please also use the API codebase to interact with a configured version of this service via Viam SDK.
+
+## Viam Service Configuration
+
+Add notes and example JSON config here.
+
+```json
+```
+
+## Troubleshooting
+
+Add troubleshooting notes here.
---
+++ src/eventManager.py
@@ -0,0 +1,62 @@
+from typing import ClassVar, Mapping, Sequence, Any, Dict, Optional, Tuple, Final, List, cast
+from typing_extensions import Self
+from typing import Final
+
+from viam.module.types import Reconfigurable
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import ResourceName, Vector3
+from viam.resource.base import ResourceBase
+from viam.resource.types import Model, ModelFamily
+
+from viam.components.generic import Generic
+from viam.utils import ValueTypes
+
+from viam.logging import getLogger
+
+import time
+import asyncio
+
+LOGGER = getLogger(__name__)
+
+class eventManager(Generic, Reconfigurable):
+    
+    """
+    Generic component, which represents any type of component that can executes arbitrary commands
+    """
+
+
+    MODEL: ClassVar[Model] = Model(ModelFamily("viam-labs", "savcam"), "event-manager")
+    
+    # create any class parameters here, 'some_pin' is used as an example (change/add as needed)
+    some_pin: int
+
+    # Constructor
+    @classmethod
+    def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
+        my_class = cls(config.name)
+        my_class.reconfigure(config, dependencies)
+        return my_class
+
+    # Validates JSON Configuration
+    @classmethod
+    def validate(cls, config: ComponentConfig):
+        # here we validate config, the following is just an example and should be updated as needed
+        some_pin = config.attributes.fields["some_pin"].number_value
+        if some_pin == "":
+            raise Exception("A some_pin must be defined")
+        return
+
+    # Handles attribute reconfiguration
+    def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
+        # here we initialize the resource instance, the following is just an example and should be updated as needed
+        self.some_pin = int(config.attributes.fields["some_pin"].number_value)
+        return
+
+    async def do_command(
+                self,
+                command: Mapping[str, ValueTypes],
+                *,
+                timeout: Optional[float] = None,
+                **kwargs
+            ) -> Mapping[str, ValueTypes]:
+        return
