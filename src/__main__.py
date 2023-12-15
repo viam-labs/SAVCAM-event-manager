6fed07d2bd633cc10886c960172654343a8f3de8
Matt Vella <mcvella@gmail.com>
2023-12-15 15:28:38 -0500

    notifications complete (not tested)

--- src/__main__.py
+++ src/__main__.py
@@ -3,14 +3,14 @@ import sys
 
 from viam.module.module import Module
 from viam.components.generic import Generic
-from .event-manager import event-manager
+from .eventManager import eventManager
 
 async def main():
     """This function creates and starts a new module, after adding all desired resources.
     Resources must be pre-registered. For an example, see the `__init__.py` file.
     """
     module = Module.from_args()
-    module.add_model_from_registry(Generic.SUBTYPE, event-manager.MODEL)
+    module.add_model_from_registry(Generic.SUBTYPE, eventManager.MODEL)
     await module.start()
 
 if __name__ == "__main__":
--- src/notifications.py
+++ src/notifications.py
@@ -1,3 +1,18 @@
+import subprocess
+import urllib.request
+
+from email.message import EmailMessage
+
+# att|verizon|sprint|tmobile|boost|metropcs
+carrier_email_gateways = {
+    "att": "mms.att.net",
+    "verizon": "vzwpix.com",
+    "sprint": "pm.sprint.com",
+    "tmobile": "tmomail.net",
+    "boost": "myboostmobile.com",
+    "metropcs": "mymetropcs.com"
+}
+
 class NotificationSMS():
     type: str="sms"
     url: str
@@ -6,14 +21,35 @@ class NotificationSMS():
 
 class NotificationEmail():
     type: str="email"
-    url: str
+    address: str
 
 class NotificationWebhookGET():
     type: str="webhook_get"
-    address: str
+    url: str
 
 def notify(event_name:str, notification:NotificationEmail|NotificationSMS|NotificationWebhookGET):
 
-    if notification.type == "email":
-        return
+    match notification.type:
+        case "email":
+            send_email(event_name, notification, False)
+        case "sms":
+            send_email(event_name, notification, True)
+        case "webhook_get":
+            contents = urllib.request.urlopen(notification.url).read()
+
     return
+
+
+def send_email(event_name:str, notification:NotificationEmail|NotificationSMS, is_sms:bool):
+    msg = EmailMessage()
+    msg.set_content("Event triggered!")
+    msg['From'] = "savcam@viam.com"
+    if is_sms:
+        to_address = notification.phone + "@" + carrier_email_gateways[notification.carrier]
+    else:
+        to_address = notification.address
+    msg['To'] = to_address
+    msg['Subject'] = "SAVCAM event: " + event_name
+
+    sendmail_location = "/usr/sbin/sendmail"
+    subprocess.run([sendmail_location, "-t", "-oi"], input=msg.as_bytes())
